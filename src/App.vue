c1dff6a2357225964c329117fe9428cd9654bd04
Alexandre Cibot <alexandre.cibot@gmail.com>
2022-01-07 14:03:22 +0100

    add function to create notes, add tailwind styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <TodoList :list="list"/>
+    <TodoList :list="list" @send="sendNewRow"/>
+    
   </div>
 </template>
 
@@ -15,11 +16,26 @@ export default {
   },
   data() {
     return {
-      list: []
+      list: [],
     }
   },
-  async created() {
-    this.list = await (axios.get('.netlify/functions/getNotes')).then(res => res.data)    
+  created() {
+    this.fetchList()
+  },
+  methods: {
+    async fetchList() {
+      this.list = await (axios.get('.netlify/functions/getNotes')).then(res => res.data)    
+    },
+    sendNewRow(data) {
+      axios.get('.netlify/functions/createNote', {params: data})
+      .then(res => {
+        console.log('res', res)  
+        this.fetchList()
+      })
+      .catch(e => {
+        console.warn('Errorr', e)
+      })
+    }
   }
 }
 </script>
--- src/components/TodoList/index.vue
+++ src/components/TodoList/index.vue
@@ -1,20 +1,74 @@
 <template>
-  <table class="table-fixed border m-3">
-    <thead class="bg-gray-300">
-      <tr>
-        <th class="px-4 py-2" v-for="(element, index) in header" :key="index">
-          {{element}}
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="todo in list" :key="todo.id">
-        <td v-for="(element, index) in header" :key="index" class="p-4">
-          {{todo[element]}}
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div>
+    <table class="table-fixed border m-3">
+      <thead class="bg-gray-300">
+        <tr>
+          <th class="px-4 py-2" v-for="(element, index) in header" :key="index">
+            {{element}}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="todo in list" :key="todo.id">
+          <td v-for="(element, index) in header" :key="index" class="p-4">
+            <span v-if="element === 'created_at'">
+              {{new Date(todo[element]).toLocaleString()}}
+            </span>
+            <span v-else>
+              {{todo[element]}}
+            </span>
+          </td>
+        </tr>
+        <tr v-if="newRow">
+          <td v-for="(element, index) in header" :key="index" class="p-4">
+            <input
+              :disabled="['created_at', 'id'].includes(element)"
+              type="text"
+              v-model="newRowData[element]"
+              class="border"
+            >
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <template v-if="!newRow">
+      <button @click="newRow = true" class="px-3 py-2 rounded-md bg-green-500 text-white font-semibold mx-3 mt-5">
+        <div class="flex items-center">
+        <span class="mr-2">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+        </span>
+        Ajouter
+        </div>
+        
+      </button>
+    </template>
+    <template v-else>
+      <button @click="handleSendRow" class="px-3 py-2 rounded-md bg-blue-500 text-white font-semibold mx-3 mt-5">
+        <div class="flex items-center">
+           <span class="mr-2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+            </svg>
+          </span>
+          <span>Valider</span>
+        </div>
+      </button>
+      <button @click="resetNewRow" class=" px-3 py-2 rounded-md bg-red-500 text-white font-semibold mx-3 mt-5">
+        <div class="flex items-center">
+          <span class="mr-2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+              <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+            </svg>
+          </span>
+          <span>
+            Annuler
+          </span>
+        </div>
+      </button>
+    </template>
+  </div>
 </template>
 
 <script>
@@ -24,6 +78,12 @@ export default {
     list: {
       type: Array,
       default: () => []
+    },
+  },
+  data() {
+    return {
+      newRow: false,
+      newRowData: {}
     }
   },
   computed: {
@@ -33,6 +93,16 @@ export default {
       
       return Object.keys(base)
       
+    }
+  },
+  methods: {
+    handleSendRow() {
+      this.$emit('send', this.newRowData)
+      this.resetNewRow()
+    },
+    resetNewRow() {
+      this.newRow = false
+      this.newRowData = {}
     }
   }
 }
